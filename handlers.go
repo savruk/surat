9ae87c4d6931a802afe54c96a6a748a9a12b2661
Yusuf Koseoglu (aka savruk) <yusufkoseoglu287@gmail.com>
2014-10-17 22:46:06 +0200

    before yosemite

--- handlers.go
+++ handlers.go
@@ -43,39 +43,47 @@ func GetHandler(w http.ResponseWriter, r *http.Request) {
 	key := r.URL.Path
 	item, _ := instance.Get(key)
 	if string(item.Value) != "" {
-		headers := GetHeadersFromCache(instance, key)
-		SetHttpHeaders(w, headers)
-		fileType := http.DetectContentType(item.Value)
-
-		if strings.HasSuffix(key, ".js") || strings.HasSuffix(key, ".css") || strings.Contains(fileType, "image") {
-			w.WriteHeader(http.StatusNotModified)
-		} else {
-			w.Write(item.Value)
-		}
+		fromCacheGetHandler(w, r, instance, key, item)
 	} else {
-		url := strings.Join([]string{fmt.Sprintf("%s://%s:%d", conf.Backend.Protocol, conf.Backend.Host, conf.Backend.Port), key}, "")
-		resp, err := http.Get(url)
-		if err != nil {
-			log.Println(err)
-		}
-		defer resp.Body.Close()
-
-		if resp.StatusCode >= 400 {
-			http.Error(w, resp.Status, resp.StatusCode)
-		} else {
-			body, _ := ioutil.ReadAll(resp.Body)
-			memerr := instance.Set(key, []byte(body))
-			if memerr != nil {
-				log.Println(memerr)
-			}
-			headers := ConvertHeaders(resp.Header)
-			SetHeadersToCache(instance, key, headers)
-			SetHttpHeaders(w, headers)
-			w.Write(body)
-		}
+		fromSourceGetHandler(w, r, instance, key)
 	}
 }
 
 func PostHandler(w http.ResponseWriter, r *http.Request) {
 	log.Panicln("Post is not supported")
+}
+
+func fromCacheGetHandler(w http.ResponseWriter, r *http.Request, instance cacher.Cacher, key string, item *cacher.Item) {
+	headers := GetHeadersFromCache(instance, key)
+	SetHttpHeaders(w, headers)
+	fileType := http.DetectContentType(item.Value)
+
+	if strings.HasSuffix(key, ".js") || strings.HasSuffix(key, ".css") || strings.Contains(fileType, "image") {
+		w.WriteHeader(http.StatusNotModified)
+	} else {
+		w.Write(item.Value)
+	}
+}
+
+func fromSourceGetHandler(w http.ResponseWriter, r *http.Request, instance cacher.Cacher, key string) {
+	url := strings.Join([]string{fmt.Sprintf("%s://%s:%d", conf.Backend.Protocol, conf.Backend.Host, conf.Backend.Port), key}, "")
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Println(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= 400 {
+		http.Error(w, resp.Status, resp.StatusCode)
+	} else {
+		body, _ := ioutil.ReadAll(resp.Body)
+		memerr := instance.Set(key, []byte(body))
+		if memerr != nil {
+			log.Println(memerr)
+		}
+		headers := ConvertHeaders(resp.Header)
+		SetHeadersToCache(instance, key, headers)
+		SetHttpHeaders(w, headers)
+		w.Write(body)
+	}
 }
